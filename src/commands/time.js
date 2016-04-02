468b10dd552a5020e403134454c78a8b8f36dbb7
RoCky Wu <rwu@cepave.com>
2016-04-02 21:11:50 +0800

    fix command: /time  format to 24 hours

--- src/commands/time.js
+++ src/commands/time.js
@@ -3,18 +3,16 @@ const ai = require('../ai')
 const got = require('got')
 const cheerio = require('cheerio')
 const moment = require('moment')
-const fs = require('fs')
 const util = require('../utils')
 
 module.exports = (msg, match) =>{
   const fromId = msg.from.id
   const chatId = msg.chat.id
   const D = moment.unix(msg.date)
-  const hhmm = D.format('hhmm')
-
+  const HHmm = D.format('HHmm')
   const srcImgs = [
-    `http://www.clockm.com/tw/img/clk/hour/${hhmm}.jpg`,
-    `http://www.bijint.com/assets/pict/jp/pc/${hhmm}.jpg`
+    `http://www.clockm.com/tw/img/clk/hour/${HHmm}.jpg`,
+    `http://www.bijint.com/assets/pict/jp/pc/${HHmm}.jpg`
   ]
 
   ai.sendMessage(chatId, util.randomArray(srcImgs))
