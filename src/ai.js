44d23e44758f392b0bf5c7d8ee015ccd9977ffdb
RoCky Wu <rwu@cepave.com>
2016-04-02 11:07:50 +0800

    Add `CepaveAIDev_bot` for dev

--- src/ai.js
+++ src/ai.js
@@ -1,4 +1,10 @@
-const token = '207182742:AAGrB9-Ok20tYqv_u7DrJHrORfUIVJkY_oQ'
+const token = process.NODE_ENV === 'production'
+  // CepaveAI_bot
+  ? '207182742:AAGrB9-Ok20tYqv_u7DrJHrORfUIVJkY_oQ'
+  
+  // CepaveAIDev_bot
+  : '160573493:AAE6g3u8NFNqQXwDZPrHWuQIg2wKPnyX94A'
+
 const TelegramBot = require('node-telegram-bot-api')
 
 const ai = new TelegramBot(token, {polling: true})
