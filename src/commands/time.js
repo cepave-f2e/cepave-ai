cf022e625497dfdbd2c2cb96c118a0c26b062b28
RoCky Wu <rwu@cepave.com>
2016-05-17 15:18:00 +0800

    fix VPS time zone issue

--- src/commands/time.js
+++ src/commands/time.js
@@ -2,14 +2,14 @@
 const ai = require('../ai')
 const got = require('got')
 const cheerio = require('cheerio')
-const moment = require('moment')
 const util = require('../utils')
+const getNow = require('../utils/now')
 
 module.exports = (msg, match) =>{
   const fromId = msg.from.id
   const chatId = msg.chat.id
-  const D = moment.unix(msg.date)
-  const HHmm = D.format('HHmm')
+  const now = getNow()
+  const HHmm = now.format('HHmm')
   const srcImgs = [
     `http://www.clockm.com/tw/img/clk/hour/${HHmm}.jpg`,
     `http://www.bijint.com/assets/pict/jp/pc/${HHmm}.jpg`
